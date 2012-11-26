6598c653386cdafc13fc37f04caf49ad5a3b5715
Andre Fonseca <aoqfonseca@gmail.com>
2012-11-26 09:27:19 -0200

    add script to run test server and make some changes

--- lib/tropeco.rb
+++ lib/tropeco.rb
@@ -2,19 +2,16 @@ require 'tropeco/plugins/hello_world'
 require 'tropeco/plugins/download'
 
 
-class Tropeco
- class_eval do
-    def start
-      bot = Cinch::Bot.new do
-        configure do |c|
-          c.server = "irc.freenode.org"
-          c.nick = "tropeco"
-          c.channels = ["#opaopa"]
-          c.plugins.plugins = [Tropeco::Plugins::HelloWorld, Tropeco::Plugins::DownloadFile]
-        end
+module Tropeco
+  def self.start
+    bot = Cinch::Bot.new do
+      configure do |c|
+        c.server = "irc.freenode.org"
+        c.nick = "tropeco"
+        c.channels = ["#opaopa"]
+        c.plugins.plugins = [Tropeco::Plugins::HelloWorld, Tropeco::Plugins::DownloadFile]
       end
-
-      bot.start
     end
+    bot.start
  end
 end
