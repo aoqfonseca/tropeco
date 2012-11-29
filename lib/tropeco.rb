397d1d22b25390bf734bc300352537a3863e3aed
Andre Fonseca <aoqfonseca@gmail.com>
2012-11-28 23:01:15 -0200

    new plugin

--- lib/tropeco.rb
+++ lib/tropeco.rb
@@ -1,5 +1,6 @@
 require 'tropeco/plugins/hello_world'
 require 'tropeco/plugins/download'
+require 'tropeco/plugins/transcriptions'
 
 
 module Tropeco
@@ -9,7 +10,7 @@ module Tropeco
         c.server = "irc.freenode.org"
         c.nick = "tropeco"
         c.channels = ["#opaopa"]
-        c.plugins.plugins = [Tropeco::Plugins::HelloWorld, Tropeco::Plugins::DownloadFile]
+        c.plugins.plugins = [Tropeco::Plugins::HelloWorld, Tropeco::Plugins::DownloadFile,Tropeco::Plugins::Transcription]
       end
     end
     bot.start
---
+++ lib/tropeco/plugins/transcriptions.rb
@@ -0,0 +1,17 @@
+require 'cinch'
+
+module Tropeco
+  module Plugins
+    class Transcription
+      include Cinch::Plugin
+
+      listen_to :message
+
+      def listen(m)
+          puts "&lt;#{m.user.nick}&gt; #{m.message}"
+      end
+
+    end
+  end
+end
+
